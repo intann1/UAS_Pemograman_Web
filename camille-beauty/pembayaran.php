0c78239e06277105906273bc73c7d0eb88d6e0e4
intann1 <150524113+intann1@users.noreply.github.com>
2026-01-30 23:57:13 +0700

    Update pembayaran.php

--- camille-beauty/pembayaran.php
+++ camille-beauty/pembayaran.php
@@ -1,15 +1,24 @@
 <?php
-$metode = $_GET['metode'];
+session_start();
+
+$nama   = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$metode = $_POST['metode'];
+$total  = $_POST['total'];
 ?>
 
-<h2>Silakan Bayar</h2>
+<h2>Pembayaran</h2>
 
-<?php if($metode == 'Transfer'){ ?>
-    <img src="qris.jpeg" width="250">
-<?php } elseif($metode == 'E-Wallet'){ ?>
-    <img src="qris.jpeg" width="250">
-<?php } else { ?>
-    <p>Bayar di tempat (COD)</p>
-<?php } ?>
+<p>Nama: <?= $nama ?></p>
+<p>Alamat: <?= $alamat ?></p>
+<p>Total Bayar: <b>Rp <?= number_format($total) ?></b></p>
 
-<p>Setelah bayar, pesanan akan diproses.</p>
+<?php if($metode == "Transfer"): ?>
+    <h3>Scan QR untuk Transfer</h3>
+    <img src="qris.jpeg" width="200">
+<?php elseif($metode == "E-Wallet"): ?>
+    <h3>Scan QR E-Wallet</h3>
+    <img src="qris.jpeg" width="200">
+<?php else: ?>
+    <p>Silakan bayar di tempat (COD)</p>
+<?php endif; ?>
